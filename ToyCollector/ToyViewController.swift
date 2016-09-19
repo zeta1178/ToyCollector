62715fc064ba94e32a10f41076fea995defa0127
Michael Cruz <michaelcruz@aim.com>
2016-09-19 12:03:51 -0400

    learning dictionaries

---
+++ ToyCollector/ToyViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ToyViewController.swift
+//  ToyCollector
+//
+//  Created by Michael Cruz on 9/19/16.
+//  Copyright © 2016 MIchael Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ToyViewController: UIViewController {
+
+    @IBOutlet weak var toyimageView: UIImageView!
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func photosTapped(_ sender: AnyObject) {
+    }
+
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+    
+}
